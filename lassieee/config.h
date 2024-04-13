41ffe16f7a2c0f61da35b537476c88a5a8581822
tms-gvd <thomas.gaviard@gmail.com>
2024-04-13 20:29:25 +0100

    remove RESET key layer 2 as I couldnt make it works with Helios controller

--- lassieee/config.h
+++ lassieee/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 #undef TAPPING_TERM
 // #define TAPPING_TERM 240
